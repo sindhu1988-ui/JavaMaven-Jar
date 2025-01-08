8a10f8da32b22b9c30a579078e00afc3db6ef7e5
sindhu <sindhu@dgv.com>
2025-01-09 00:35:11 +0530

    Initial commit for JavaMaven-Jar project

---
+++ src/main/java/com/example/JavaMaven_Jar/JavaMavenJarApplication.java
@@ -0,0 +1,13 @@
+package com.example.JavaMaven_Jar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMavenJarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMavenJarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JavaMaven_Jar/JavaMavenJarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaMaven_Jar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMavenJarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
